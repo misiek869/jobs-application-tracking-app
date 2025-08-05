6fc6cb380a064b8de93d67a30b2259c9caf33bf2
Michal Gawik <gawlik.zator@o2.pl>
2025-08-05 06:04:35 +0200

    setup landing page structure

--- src/pages/HomeLayout.tsx
+++ src/pages/HomeLayout.tsx
@@ -3,10 +3,9 @@ import { Outlet } from 'react-router'
 
 const HomeLayout = () => {
 	return (
-		<div>
-			<h1>Home</h1>
+		<>
 			<Outlet />
-		</div>
+		</>
 	)
 }
 
--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,7 +1,36 @@
-import React from 'react'
+import styled from 'styled-components'
+import main from '../assets/images/main.svg'
+import logo from '../assets/images/logo.svg'
+import { Link } from 'react-router'
 
 const Landing = () => {
-	return <div>Landing</div>
+	return (
+		<Wrapper>
+			<nav>
+				<img src={logo} alt='jobify' className='logo' />
+			</nav>
+			<div className='container page'>
+				<div className='info'>
+					<h1>
+						application <span>tracking app</span>
+					</h1>
+					<p>
+						Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+						Reprehenderit qui porro quibusdam voluptatem officiis rem vitae
+						placeat exercitationem libero nemo. Ab maxime illo doloremque
+						officiis, vel perferendis rerum quasi necessitatibus.
+					</p>
+					<Link to={'/register'} className='btn register-link'>
+						Register
+					</Link>
+					<Link to={'/login'} className='btn register-link'>
+						Login | Demo User
+					</Link>
+				</div>
+				<img src={main} className='img main-img' alt='' />
+			</div>
+		</Wrapper>
+	)
 }
 
 export default Landing
