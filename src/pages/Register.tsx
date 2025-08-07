f04c89cc5334f01348cfcb76c81838d22bee0ad8
Michal Gawik <gawlik.zator@o2.pl>
2025-08-07 05:54:05 +0200

    create page structure

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,12 +1,37 @@
-import React from 'react'
+import Wrapper from '../assets/wrappers/RegisterPage'
 import { Link } from 'react-router'
+import { Logo } from '../components'
 
 const Register = () => {
 	return (
-		<div>
-			<h1>register</h1>
-			<Link to='/login'>Login Page</Link>;
-		</div>
+		<Wrapper>
+			<form className='form'>
+				<Logo />
+				<h4>Register</h4>
+				<div className='form-row'>
+					<label htmlFor='name' className='form-label'>
+						name
+					</label>
+					<input
+						type='text'
+						id='name'
+						name='name'
+						className='form-input'
+						defaultValue={'Michael'}
+						required
+					/>
+				</div>
+				<button type='submit' className='btn btn-block'>
+					submit
+				</button>
+				<p>
+					Already have an account?
+					<Link to={'/login'} className='member-btn'>
+						Login
+					</Link>
+				</p>
+			</form>
+		</Wrapper>
 	)
 }
 
