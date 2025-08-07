559fc2fb34d8c6c5dd046c08b144ee74f2a75a15
Michal Gawik <gawlik.zator@o2.pl>
2025-08-07 06:06:20 +0200

    import FormRow component and use

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,6 +1,6 @@
 import Wrapper from '../assets/wrappers/RegisterPage'
 import { Link } from 'react-router'
-import { Logo } from '../components'
+import { FormRow, Logo } from '../components'
 
 const Register = () => {
 	return (
@@ -8,7 +8,16 @@ const Register = () => {
 			<form className='form'>
 				<Logo />
 				<h4>Register</h4>
-
+				<FormRow type='text' name='name' defaultValue='michael' />
+				<FormRow
+					type='text'
+					name='lastName'
+					labelText='last name'
+					defaultValue='gawlik'
+				/>
+				<FormRow type='text' name='location' defaultValue='oświęcim' />
+				<FormRow type='email' name='email' defaultValue='michael@michael.com' />
+				<FormRow type='password' name='password' defaultValue='1234' />
 				<button type='submit' className='btn btn-block'>
 					submit
 				</button>
