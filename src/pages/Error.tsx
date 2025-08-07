5e3c9742ce1c1a7c7cf8ee6e7eeb458c48b9ce64
Michal Gawik <gawlik.zator@o2.pl>
2025-08-07 05:23:42 +0200

    create and style error page

--- src/pages/Error.tsx
+++ src/pages/Error.tsx
@@ -1,14 +1,28 @@
 import { Link, useRouteError } from 'react-router'
 import Wrapper from '../assets/wrappers/ErrorPage'
+import notFoundImg from '../assets/images/not-found.svg'
 
 const Error = () => {
 	const error = useRouteError()
 
+	if (error.status === 404) {
+		return (
+			<Wrapper>
+				<div>
+					<img src={notFoundImg} alt='not found image' />
+					<h3>Page not found</h3>
+					<Link to={'/dashboard'}>Go Back</Link>
+				</div>
+			</Wrapper>
+		)
+	}
+
 	return (
-		<div>
-			<h1>Error</h1>
-			<Link to={'/'}>Home</Link>
-		</div>
+		<Wrapper>
+			<div>
+				<h3>Something went wrong</h3>
+			</div>
+		</Wrapper>
 	)
 }
 
