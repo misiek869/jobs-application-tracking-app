cc3e026ea34b03a97ba818f66c991e19567e3edb
Michal Gawik <gawlik.zator@o2.pl>
2025-08-10 06:51:04 +0200

    create login page

---
+++ src/assets/wrappers/LoginPage.tsx
@@ -0,0 +1,14 @@
+import styled from 'styled-components'
+
+const Wrapper = styled.section`
+	min-height: 100vh;
+	display: grid;
+	align-items: center;
+
+	.logo {
+		display: block;
+		margin: 0 auto;
+	}
+`
+
+export default Wrapper
