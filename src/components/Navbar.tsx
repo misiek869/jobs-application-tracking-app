91a0061c8219fec5506944eb4f3d4617c39b88f1
Michal Gawik <gawlik.zator@o2.pl>
2025-08-21 07:21:49 +0200

    create structure and context

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,26 @@
 import React from 'react'
 import Wrapper from '../assets/wrappers/Navbar'
+import { FaAlignLeft } from 'react-icons/fa6'
+import Logo from './Logo'
+import { useDashboardContext } from '../pages/DashboardLayout'
 
 const Navbar = () => {
-	return <div>Navbar</div>
+	const { toggleSidebar } = useDashboardContext()
+
+	return (
+		<Wrapper>
+			<div className='nav-center'>
+				<button className='toggle-btn' onClick={toggleSidebar}>
+					<FaAlignLeft />
+				</button>
+				<div>
+					<Logo />
+					<h4 className='logo-text'>dashboard</h4>
+				</div>
+				<div className='btn-container'>toggle/logout</div>
+			</div>
+		</Wrapper>
+	)
 }
 
 export default Navbar
