24a365259f66f5e2afcf976f9c73859741fbabff
Michal Gawik <gawlik.zator@o2.pl>
2025-08-22 06:10:03 +0200

    set structure

--- src/components/SmallSidebar.tsx
+++ src/components/SmallSidebar.tsx
@@ -1,8 +1,38 @@
 import React from 'react'
 import Wrapper from '../assets/wrappers/SmallSidebar'
+import { useDashboardContext } from '../pages/DashboardLayout'
+import { IoClose } from 'react-icons/io5'
+import Logo from './Logo'
+import links from '../utils/links'
+import { NavLink } from 'react-router'
 
 const SmallSidebar = () => {
-	return <Wrapper>SmallSidebar</Wrapper>
+	const data = useDashboardContext()
+
+	return (
+		<Wrapper>
+			<div className='sidebar-container show-sidebar'>
+				<div className='content'>
+					<button className='close-btn'>
+						<IoClose />
+					</button>
+					<header>
+						<Logo />
+					</header>
+					<div className='nav-links'>
+						{links.map(link => {
+							const { text, icon, path } = link
+							return (
+								<NavLink to={path} key={text} className='nav-link'>
+									<span className='icon'>{icon}</span> {text}
+								</NavLink>
+							)
+						})}
+					</div>
+				</div>
+			</div>
+		</Wrapper>
+	)
 }
 
 export default SmallSidebar
