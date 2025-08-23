8dcf794e6be2bb955e2cf0665efae96a59766962
Michal Gawik <gawlik.zator@o2.pl>
2025-08-23 06:36:37 +0200

    create component

---
+++ src/components/LogoutContainer.tsx
@@ -0,0 +1,8 @@
+import { FaRegUser } from 'react-icons/fa'
+import { FaCaretDown } from 'react-icons/fa6'
+
+const LogoutContainer = () => {
+	return <div></div>
+}
+
+export default LogoutContainer
