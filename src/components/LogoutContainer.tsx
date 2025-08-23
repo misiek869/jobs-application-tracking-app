96a9bbf5c0f5ce53580a5e829095b6747e6c0d36
Michal Gawik <gawlik.zator@o2.pl>
2025-08-23 06:41:23 +0200

    import LogoutContainer

--- src/components/LogoutContainer.tsx
+++ src/components/LogoutContainer.tsx
@@ -1,8 +1,22 @@
 import { FaRegUser } from 'react-icons/fa'
 import { FaCaretDown } from 'react-icons/fa6'
+import Wrapper from '../assets/wrappers/LogoutContainer'
+import { useDashboardContext } from '../pages/DashboardLayout'
+import { useState } from 'react'
 
 const LogoutContainer = () => {
-	return <div></div>
+	const [showLogout, setShowLogout] = useState<boolean>(false)
+	const { user, logout } = useDashboardContext()
+
+	return (
+		<Wrapper>
+			<button
+				className='btn logout-btn'
+				onClick={() => {
+					setShowLogout(!showLogout)
+				}}></button>
+		</Wrapper>
+	)
 }
 
 export default LogoutContainer
